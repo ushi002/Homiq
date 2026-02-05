bd766a4500de71f7c8ea1eb4cc08b75285f799b8
ludek <uhlir@ufa.cas.cz>
2026-02-05 16:41:29 +0100

    Redirect /buildings and /units to dashboard

---
+++ frontend/src/app/buildings/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+
+export default function BuildingsIndex() {
+    const router = useRouter();
+
+    useEffect(() => {
+        router.replace('/');
+    }, [router]);
+
+    return null;
+}
